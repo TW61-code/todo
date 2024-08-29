12f203a8e86500cb93f5ee92df7170c6f03f37d3
Kai Mitchell <kaimitchell.personal@gmail.com>
2024-08-29 22:35:06 +1200

    Implement code to apply styling to a selected task.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
 
-  const [todos, setTodos] = useState([]);
   const [input, setValue] = useState();
+  const [todos, setTodos] = useState(() => {
+    let storedTodos = localStorage.getItem('TODOS'); 
+    return storedTodos ? JSON.parse(storedTodos) : []
+  });
+
+  useEffect(() => {
+    console.log('saving todos to local storage', todos);
+    localStorage.setItem('TODOS', JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(e) {
     e.preventDefault();
-    //add an object and it's property to existing array.
-    setTodos(prev => [...prev, {text: input}]);
+
+    if (input === "") {
+      return;
+    } else {
+      //add an object and it's property to existing array.
+      setTodos(prev => [...prev, {text: input, id: new Date()}]);
+      setValue("");
+    }
   }
 
-  function updateTodos(id) {
-    setTodos(todos.filter(todo => todo.text !== id));
+  function deleteTodos(e, id) {
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  function completeTodos(e, id) {
+    e.preventDefault();
+    const completedTodo = todos.find((todo) => todo.id === id);
+    //Figure out how I can change the specific todo items styling to indicate that the the task is complete.
   }
 
   return (
-    <form onSubmit={addTodo}>
-      <input value={input} onChange={(e) => setValue(e.target.value)} />
-      <button>Submit</button><br></br>
+    <div className="todo-app">
+      <h1>TODO</h1>
+      <form onSubmit={addTodo}>
+        <input type="text" value={input} onChange={(e) => setValue(e.target.value)} />
+        <button>Submit</button>
+      </form>
       <ul>
-        {todos.map((todo, index) => {
-          return (
-            <li key={index}>
-              {todo.text}
-              <button onClick={() => updateTodos(todo.text)}>Delete</button>
-            </li>
-          )
-        })}
-      </ul>
-    </form>
+          {todos.map((todo, index) => {
+            return (
+              <li key={index}>
+                {todo.text}
+                <button onClick={(e) => completeTodos(e, todo.id)}>Complete</button>
+                <button onClick={(e) => deleteTodos(e, todo.id)}>Delete</button>
+              </li>
+            )
+          })}
+        </ul>
+    </div>
   )
 
 }
