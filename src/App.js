8f555f34eed276f5121a769bc609b3d4bd0f10fc
Kai Mitchell <KaiMitchell@gmail.com>
2024-08-29 16:54:31 +1200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [input, setValue] = useState();
+
+  function addTodo(e) {
+    e.preventDefault();
+    //add an object and it's property to existing array.
+    setTodos(prev => [...prev, {text: input}]);
+  }
+
+  function updateTodos(id) {
+    setTodos(todos.filter(todo => todo.text !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={addTodo}>
+      <input value={input} onChange={(e) => setValue(e.target.value)} />
+      <button>Submit</button><br></br>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              {todo.text}
+              <button onClick={() => updateTodos(todo.text)}>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    </form>
+  )
+
 }
 
 export default App;
