c4883da1044c225130d5cbff858868a009c3450d
Kai Mitchell <kaimitchell.personal@gmail.com>
2024-08-30 19:48:06 +1200

    Complete all the event handlers for bonus challenge. I must revent multiple edits being called.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import {useState, useEffect} from "react";
 
+const completedStyles = {
+  color: "hsl(0, 0%, 60%)",
+  textDecoration: "line-through",
+}
+
 function App() {
 
+  const [editInput, setEditValue] = useState('');
   const [input, setValue] = useState();
   const [todos, setTodos] = useState(() => {
     let storedTodos = localStorage.getItem('TODOS'); 
@@ -21,22 +27,61 @@ function App() {
       return;
     } else {
       //add an object and it's property to existing array.
-      setTodos(prev => [...prev, {text: input, id: new Date()}]);
+      setTodos(prev => [...prev, {
+        text: input, 
+        id: new Date(), 
+        completed: false,
+        isEdit: false,
+      }]);
       setValue("");
     }
   }
 
-  function deleteTodos(e, id) {
+  function handleDeleteTodo(e, id) {
     e.preventDefault();
     setTodos((currentTodos) => {
       return currentTodos.filter((todo) => todo.id !== id);
     });
   };
 
-  function completeTodos(e, id) {
+  function handleCompleteTodo(e, id) {
     e.preventDefault();
-    const completedTodo = todos.find((todo) => todo.id === id);
-    //Figure out how I can change the specific todo items styling to indicate that the the task is complete.
+    
+    const completedTodos = todos.map((todo) => {
+      return todo.id === id ? {...todo, completed: !todo.completed} : todo;
+    });
+    setTodos(completedTodos);
+  }
+
+  function handleEditTodo(e, id) {
+    e.preventDefault();
+    const updateTodos = todos.map((todo) => {
+        return todo.id === id ? {...todo, isEdit: !todo.isEdit} : todo;
+    });
+    console.log(updateTodos);
+    setTodos(updateTodos);
+  }
+
+  function handleSubmitEdit(e, id) {
+    e.preventDefault();
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id && editInput !== '') {
+        return {...todo, text: editInput, isEdit: false};
+      } else {
+        return {...todo, text: todo.text, isEdit: false};
+      }
+    });
+    setTodos(updateTodos);
+    setEditValue('');
+  }
+
+  function handleCancelEdit(e, id) {
+    e.preventDefault();
+    const updateTodos = todos.map((todo) => {
+      return todo.id === id ? {...todo, isEdit: false} : todo;
+    });
+    setTodos(updateTodos);
+    setEditValue('');
   }
 
   return (
@@ -50,9 +95,21 @@ function App() {
           {todos.map((todo, index) => {
             return (
               <li key={index}>
-                {todo.text}
-                <button onClick={(e) => completeTodos(e, todo.id)}>Complete</button>
-                <button onClick={(e) => deleteTodos(e, todo.id)}>Delete</button>
+                {todo.isEdit ? 
+                  <div className='isEdit'>
+                    <input type='text' value={editInput} onChange={(e) => setEditValue(e.target.value)}/> 
+                    <span onClick={(e) => handleSubmitEdit(e, todo.id)}>✔️</span>
+                    <span onClick={(e) => handleCancelEdit(e, todo.id)}>❌</span>   
+                  </div>
+                  : 
+                  <div className='isNotEdit'>
+                    <p style={todo.completed ? completedStyles : {}}>{todo.text}</p>
+                    <div className="todo-span-container">
+                      <span onClick={(e) => handleEditTodo(e, todo.id)}>✏️</span>
+                      <span onClick={(e) => handleCompleteTodo(e, todo.id)}>✔️</span>
+                      <span onClick={(e) => handleDeleteTodo(e, todo.id)}>❌</span>   
+                    </div>
+                  </div>}
               </li>
             )
           })}
